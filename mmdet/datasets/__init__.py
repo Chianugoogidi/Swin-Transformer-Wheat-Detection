62dd6f7585c11f9c4d13e93719cb0b4b529b9ff4
Franklin C. Ogidi <gmi672@usask.ca>
2021-07-09 15:30:30 -0600

    added config for swin-transformer model for GWHD

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -17,7 +17,7 @@ from .xml_style import XMLDataset
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
     'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
-    'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
+    'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler', 'WheatDataset',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
     'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
