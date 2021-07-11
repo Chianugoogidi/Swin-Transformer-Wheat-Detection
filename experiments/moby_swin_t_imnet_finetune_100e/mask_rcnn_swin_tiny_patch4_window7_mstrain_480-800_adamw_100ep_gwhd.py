402a549aa128db92be0604d66a8ca34360eeea54
Franklin C. Ogidi <gmi672@usask.ca>
2021-07-10 22:15:41 -0600

    added solution notebooks

---
+++ experiments/moby_swin_t_imnet_finetune_100e/mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_adamw_100ep_gwhd.py
@@ -0,0 +1,452 @@
+model = dict(
+    type='MaskRCNN',
+    pretrained=
+    'https://github.com/SwinTransformer/storage/releases/download/v1.0.3/moby_swin_t_300ep_pretrained.pth',
+    backbone=dict(
+        type='SwinTransformer',
+        embed_dim=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        ape=False,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        use_checkpoint=False,
+        frozen_stages=-1),
+    neck=dict(
+        type='FPN',
+        in_channels=[96, 192, 384, 768],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        mask_roi_extractor=None,
+        mask_head=None),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            mask_size=28,
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100,
+            mask_thr_binary=0.5)))
+dataset_type = 'WheatDataset'
+data_root = 'data/gwhd/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+albu_train_transforms = [
+    dict(
+        type='ShiftScaleRotate',
+        shift_limit=0.0625,
+        scale_limit=0.0,
+        rotate_limit=0,
+        interpolation=1,
+        p=0.5),
+    dict(type='Flip', p=0.5),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='GaussNoise', var_limit=50.0, p=0.5),
+            dict(type='ISONoise', intensity=(0.2, 0.8), p=0.5),
+            dict(type='MultiplicativeNoise', multiplier=(0.5, 1.1), p=0.5),
+            dict(type='NoOp')
+        ],
+        p=0.5),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='GaussianBlur', p=0.5),
+            dict(type='Blur', p=0.5),
+            dict(type='NoOp')
+        ],
+        p=0.5),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='ToGray', p=0.5),
+            dict(type='RGBShift', p=0.5),
+            dict(type='NoOp')
+        ],
+        p=0.5),
+    dict(
+        type='HueSaturationValue',
+        hue_shift_limit=20,
+        sat_shift_limit=30,
+        val_shift_limit=20,
+        p=0.5),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='RandomGamma', p=0.5),
+            dict(type='CLAHE', p=0.5),
+            dict(
+                type='RandomBrightnessContrast',
+                brightness_limit=[0.1, 0.3],
+                contrast_limit=[0.1, 0.3],
+                p=0.5)
+        ],
+        p=0.5)
+]
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Pad', size_divisor=32),
+    dict(
+        type='Albu',
+        transforms=[
+            dict(
+                type='ShiftScaleRotate',
+                shift_limit=0.0625,
+                scale_limit=0.0,
+                rotate_limit=0,
+                interpolation=1,
+                p=0.5),
+            dict(type='Flip', p=0.5),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(type='GaussNoise', var_limit=50.0, p=0.5),
+                    dict(type='ISONoise', intensity=(0.2, 0.8), p=0.5),
+                    dict(
+                        type='MultiplicativeNoise',
+                        multiplier=(0.5, 1.1),
+                        p=0.5),
+                    dict(type='NoOp')
+                ],
+                p=0.5),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(type='GaussianBlur', p=0.5),
+                    dict(type='Blur', p=0.5),
+                    dict(type='NoOp')
+                ],
+                p=0.5),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(type='ToGray', p=0.5),
+                    dict(type='RGBShift', p=0.5),
+                    dict(type='NoOp')
+                ],
+                p=0.5),
+            dict(
+                type='HueSaturationValue',
+                hue_shift_limit=20,
+                sat_shift_limit=30,
+                val_shift_limit=20,
+                p=0.5),
+            dict(
+                type='OneOf',
+                transforms=[
+                    dict(type='RandomGamma', p=0.5),
+                    dict(type='CLAHE', p=0.5),
+                    dict(
+                        type='RandomBrightnessContrast',
+                        brightness_limit=[0.1, 0.3],
+                        contrast_limit=[0.1, 0.3],
+                        p=0.5)
+                ],
+                p=0.5)
+        ],
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            min_area=0.0,
+            filter_lost_elements=True),
+        keymap=dict(img='image', gt_bboxes='bboxes'),
+        update_pad_shape=False,
+        skip_img_without_anno=True),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='DefaultFormatBundle'),
+    dict(
+        type='Collect',
+        keys=['img', 'gt_bboxes', 'gt_labels'],
+        meta_keys=('filename', 'ori_shape', 'img_shape', 'img_norm_cfg',
+                   'pad_shape', 'scale_factor'))
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type='WheatDataset',
+        ann_file='data/gwhd/annotations/train.json',
+        img_prefix='data/gwhd/train/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+            dict(type='Pad', size_divisor=32),
+            dict(
+                type='Albu',
+                transforms=[
+                    dict(
+                        type='ShiftScaleRotate',
+                        shift_limit=0.0625,
+                        scale_limit=0.0,
+                        rotate_limit=0,
+                        interpolation=1,
+                        p=0.5),
+                    dict(type='Flip', p=0.5),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(type='GaussNoise', var_limit=50.0, p=0.5),
+                            dict(type='ISONoise', intensity=(0.2, 0.8), p=0.5),
+                            dict(
+                                type='MultiplicativeNoise',
+                                multiplier=(0.5, 1.1),
+                                p=0.5),
+                            dict(type='NoOp')
+                        ],
+                        p=0.5),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(type='GaussianBlur', p=0.5),
+                            dict(type='Blur', p=0.5),
+                            dict(type='NoOp')
+                        ],
+                        p=0.5),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(type='ToGray', p=0.5),
+                            dict(type='RGBShift', p=0.5),
+                            dict(type='NoOp')
+                        ],
+                        p=0.5),
+                    dict(
+                        type='HueSaturationValue',
+                        hue_shift_limit=20,
+                        sat_shift_limit=30,
+                        val_shift_limit=20,
+                        p=0.5),
+                    dict(
+                        type='OneOf',
+                        transforms=[
+                            dict(type='RandomGamma', p=0.5),
+                            dict(type='CLAHE', p=0.5),
+                            dict(
+                                type='RandomBrightnessContrast',
+                                brightness_limit=[0.1, 0.3],
+                                contrast_limit=[0.1, 0.3],
+                                p=0.5)
+                        ],
+                        p=0.5)
+                ],
+                bbox_params=dict(
+                    type='BboxParams',
+                    format='pascal_voc',
+                    label_fields=['gt_labels'],
+                    min_visibility=0.0,
+                    min_area=0.0,
+                    filter_lost_elements=True),
+                keymap=dict(img='image', gt_bboxes='bboxes'),
+                update_pad_shape=False,
+                skip_img_without_anno=True),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='DefaultFormatBundle'),
+            dict(
+                type='Collect',
+                keys=['img', 'gt_bboxes', 'gt_labels'],
+                meta_keys=('filename', 'ori_shape', 'img_shape',
+                           'img_norm_cfg', 'pad_shape', 'scale_factor'))
+        ]),
+    val=dict(
+        type='WheatDataset',
+        ann_file='data/gwhd/annotations/val.json',
+        img_prefix='data/gwhd/train/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='DefaultFormatBundle'),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
+    test=dict(
+        type='WheatDataset',
+        ann_file='data/gwhd/annotations/val.json',
+        img_prefix='data/gwhd/train/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(1333, 800),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='DefaultFormatBundle'),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
+evaluation = dict(interval=1, metric='bbox')
+optimizer = dict(
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            absolute_pos_embed=dict(decay_mult=0.0),
+            relative_position_bias_table=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0))))
+optimizer_config = dict(
+    grad_clip=None,
+    type='DistOptimizerHook',
+    update_interval=1,
+    coalesce=True,
+    bucket_size_mb=-1,
+    use_fp16=True)
+lr_config = dict(
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=4,
+    warmup_ratio=0.1,
+    min_lr=1e-06,
+    warmup_by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
+custom_hooks = [dict(type='NumClassCheckHook')]
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+fp16 = None
+work_dir = 'experiments/moby_swin_t_imnet_finetune_100e'
+gpu_ids = range(0, 1)
---
+++ global-wheat-challenge/README.md
@@ -0,0 +1,18 @@
+# 31st Place Solution for the 2021 Global Wheat Challenge
+
+This solution finetunes a Mask R-CNN model with a Swin Transformer encoder, pretrained in a self-supervised manner using the [MoBY](https://arxiv.org/abs/2105.04553) framework, on the 2021 Global Wheat Head Detection (GWHD) dataset.
+
+## Steps to Reproduce
+
+1. Run the [EDA](EDA.ipynb) notebook to download the data, split the dataset and convert annotations to the COCO format.
+2. Train the model with following command:
+
+```
+tools/dist_train.sh configs/gwhd/mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_adamw_100ep_gwhd.py 1 --work-dir experiments/moby_swin_t_imnet_finetune_100e --cfg-options model.pretrained=https://github.com/SwinTransformer/storage/releases/download/v1.0.3/moby_swin_t_300ep_pretrained.pth
+```
+
+3. Run the [inference](inference.ipynb) notebook to generate submission file and submit to the competition.
+
+## References
+
+[Transformer-SSL](https://github.com/SwinTransformer/Transformer-SSL)
